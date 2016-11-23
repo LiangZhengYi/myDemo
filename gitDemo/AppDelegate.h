078deabc374564843d757fe7a6dd1185321b7b15
mac <mac@macdemacbook-pro-4.local>
2016-11-22 19:16:28 -0800

    添加了一个文件

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by mac on 16/11/22.
+//  Copyright © 2016年 LZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by mac on 16/11/22.
+//  Copyright © 2016年 LZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
